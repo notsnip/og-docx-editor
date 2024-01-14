48a08284fcf9e37f8051d0d53b5bc0612089018b
realsnipc <snipc.mail@proton.me>
2024-01-14 22:29:53 +0530

    Update Styles

--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactQuill from 'react-quill'
-import 'react-quill/dist/quill.snow.css'
-import '../styles/Editor.style.css'
+import React from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import '../styles/Editor.style.css';
 
 export default function TextEditor() {
 
