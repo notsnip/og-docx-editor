58c4b8713fa7b8817fa78c561a4f4d9096a73624
realsnipc <snipc.mail@proton.me>
2024-01-16 17:41:56 +0530

    shift sockets to new directory

---
+++ api/src/sockets/index.socket.js
@@ -0,0 +1,18 @@
+const io = require("socket.io")(3001,{
+    cors:{
+        origin: ["http://localhost:5173", process.env.CLIENT]
+    }
+});
+
+io.on('connection', (socket)=>{
+    console.log(`A new connection has been made: ${socket.id}`);
+    socket.on('join-document',id=>{
+        socket.join(id);
+    })
+    socket.on('send-editorData',(data,documentId)=>{
+        socket.to(documentId).emit('broadcast-editorData',data);
+    });
+    
+})
+
+module.exports= io;
