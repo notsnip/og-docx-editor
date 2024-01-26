a80c039697ffd165ae5b3893af72ca4c96ffa5d9
realsnipc <snipc.mail@proton.me>
2024-01-26 11:09:23 +0530

    update action bar

--- client/src/pages/EditDocument.jsx
+++ client/src/pages/EditDocument.jsx
@@ -4,7 +4,7 @@ import "react-quill/dist/quill.snow.css";
 import "../styles/Editor.style.css";
 import { io } from "socket.io-client";
 import { useParams } from "react-router-dom";
-import ActionBar from "./components/ActionBar";
+import ActionBar from "../components/ActionBar";
 
 const server = import.meta.env.VITE_SERVER || "http://localhost:5000";
 const webSocket = io(server);
---
+++ client/src/utils/postRequest.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function postRequest() {
+  return (
+    <>postRequest</>
+  )
+}
+
+
+const postRequest= (url,body)=>{
+  
+
+}
