bbc64c8f0fdccaf0c8224fdac68bbac42ffb082f
realsnipc <snipc.mail@proton.me>
2024-01-28 17:38:06 +0530

    update envs

--- api/src/constants.js
+++ api/src/constants.js
@@ -1,3 +1,5 @@
 const CLIENT_URL = process.env.CLIENT_URL;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
 
-export {CLIENT_URL};
+export { CLIENT_URL, DB_PASS, DB_USER };
--- api/src/db/connection.js
+++ api/src/db/connection.js
@@ -1,13 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import { DB_USER, DB_PASS } from "../constants.js";
 
-
-
-
-mongoose.connect(`mongodb+srv://snipc:IchigoBankai@cluster0.xqbuymk.mongodb.net/`)
-.then(() => {
-  console.log("Succesfully Connected to MongoDB");
-})
-.catch((error) => {
-  console.log("Connection to Mongodb failed", error);
-  process.exit(1); // This will exit the program with a non-zero exit code, indicating that an error occurred.
-});
+mongoose
+  .connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.xqbuymk.mongodb.net/`)
+  .then(() => {
+    console.log("Succesfully Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Connection to Mongodb failed", error);
+    process.exit(1);
+  });
